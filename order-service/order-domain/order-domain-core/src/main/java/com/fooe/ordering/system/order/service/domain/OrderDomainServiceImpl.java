a9c9b10e879fe821d89ae0f4530476259f06ebf3
Wooyongjeong <wooyong.dev@gmail.com>
2026-01-14 15:45:40 +0900

    feat(order-domain-core): Implement OrderDomainService

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/fooe/ordering/system/order/service/domain/OrderDomainServiceImpl.java
@@ -0,0 +1,74 @@
+package com.fooe.ordering.system.order.service.domain;
+
+import com.fooe.ordering.system.order.service.domain.entity.Order;
+import com.fooe.ordering.system.order.service.domain.entity.Product;
+import com.fooe.ordering.system.order.service.domain.entity.Restaurant;
+import com.fooe.ordering.system.order.service.domain.event.OrderCancelledEvent;
+import com.fooe.ordering.system.order.service.domain.event.OrderCreatedEvent;
+import com.fooe.ordering.system.order.service.domain.event.OrderPaidEvent;
+import com.fooe.ordering.system.order.service.domain.exception.OrderDomainException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+@Slf4j
+public class OrderDomainServiceImpl implements OrderDomainService {
+
+    private static final String UTC = "UTC";
+
+    @Override
+    public OrderCreatedEvent validateAndInitiateOrder(Order order, Restaurant restaurant) {
+        validateRestaurant(restaurant);
+        setOrderProductInformation(order, restaurant);
+        order.validateOrder();
+        order.initializeOrder();
+        log.info("Order with id: {} is initiated", order.getId().getValue());
+        return new OrderCreatedEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public OrderPaidEvent payOrder(Order order) {
+        order.pay();
+        log.info("Order with id: {} is paid", order.getId().getValue());
+        return new OrderPaidEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public void approveOrder(Order order) {
+        order.approve();
+        log.info("Order with id: {} is approved", order.getId().getValue());
+    }
+
+    @Override
+    public OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages) {
+        order.initCancel(failureMessages);
+        log.info("Order payment is cancelling for order id: {}", order.getId().getValue());
+        return new OrderCancelledEvent(order, ZonedDateTime.now(ZoneId.of(UTC)));
+    }
+
+    @Override
+    public void cancelOrder(Order order, List<String> failureMessages) {
+        order.cancel(failureMessages);
+        log.info("Order with id: {} is cancelled", order.getId().getValue());
+    }
+
+    private void validateRestaurant(Restaurant restaurant) {
+        if (!restaurant.isActive()) {
+            throw new OrderDomainException("Restaurant with id " + restaurant.getId().getValue() +
+                    " is currently not active!");
+        }
+    }
+
+    private void setOrderProductInformation(Order order, Restaurant restaurant) {
+        // TODO Hashmap 사용으로 시간 복잡도 O(n)으로 줄이기
+        order.getItems().forEach(orderItem -> restaurant.getProducts().forEach(restaurantProduct -> {
+            Product currentProduct = orderItem.getProduct();
+            if (currentProduct.equals(restaurantProduct)) {
+                currentProduct.updateWithConfirmedNameAndPrice(restaurantProduct.getName(),
+                        restaurantProduct.getPrice());
+            }
+        }));
+    }
+}
